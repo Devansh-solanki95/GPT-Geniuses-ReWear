43f75eece5f97fcd32089bc4fd6ce66237fadeb8
Devansh-solanki95 <devanshsolanki83@gmail.com>
2025-07-12 09:56:51 +0530

    project created

---
+++ Rewear-Clothing-Exhange/src/main/java/com/rewear/RewearClothingExhangeApplication.java
@@ -0,0 +1,13 @@
+package com.rewear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewearClothingExhangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewearClothingExhangeApplication.class, args);
+	}
+
+}
